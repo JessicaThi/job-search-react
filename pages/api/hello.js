529e650e7249874a41668db0d17aa9c94d61d33d
JessicaThi <jessica.thielemans@hetic.net>
2020-12-01 21:14:50 +0100

    install axios + api call + display the first 5 jobs

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  axios.get('https://jobs.github.com/positions.json?description=ruby&page=1').then(result => {
+    res.statusCode = 200
+    res.json(result.data)
+  })
+
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 
 export default function Home() {
+  const [job, setJob] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `/api/hello`,
+      {
+        method: "GET",
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        setJob(response)
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <main className="container max-w-screen-xl mx-auto px-5">
       <h1 className="text-2xl	font-bold py-8">Github <span className="font-light">Jobs</span></h1>
@@ -40,22 +58,24 @@ export default function Home() {
         </aside>
 
         <div className="col-span-3 lg:col-span-2">
-          <div className="flex justify-between bg-white p-2 mb-8 rounded shadow-sm">
-            <div className="w-24 h-24 flex justify-center items-center rounded">
-              <img src="https://github-jobs.s3.amazonaws.com/eDYEuyp8KWQ7XhCbzyYkaELK?response-content-disposition=inline%3B%20filename%3D%22Shells%2002.jpg%22%3B%20filename%2A%3DUTF-8%27%27Shells%252002.jpg&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJENXOYUQN2IQEWRA%2F20201128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201128T142305Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=808131e90df6116733341471559a5f3c20411f474e54de5fd49d382529a66499" />
-            </div>
-            <div className="flex flex-1 flex-col sm:flex-row">
-              <div className="flex flex-1 flex-col items-start">
-                <p className="text-xs font-roboto font-bold text-blue-900">Kasisto</p>
-                <p className="text-lg font-roboto font-normal text-blue-900 py-2">Front-End Software Engineer</p>
-                <span className="text-xs font-roboto font-bold text-blue-900 border border-blue-900 rounded py-1 px-2">Full time</span>
+          {job ? job.slice(0, 5).map(job => (
+            <div className="flex justify-between bg-white p-2 mb-8 rounded shadow-sm">
+              <div className="w-24 h-24 mr-3 flex justify-center items-center rounded">
+                <img src={job.company_logo} />
               </div>
-              <div className="flex items-end mt-4 sm:mb-2 mr-1">
-                <span className="flex items-center font-roboto font-medium text-xs text-gray-400 mr-8"><svg className="w-6 h-6 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" /></svg>New York</span>
-                <span className="flex items-center font-roboto font-medium text-xs text-gray-400"><svg className="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>5 days ago</span>
+              <div className="flex flex-1 flex-col sm:flex-row">
+                <div className="flex flex-1 flex-col items-start">
+                  <p className="text-xs font-roboto font-bold text-blue-900">{job.company}</p>
+                  <p className="text-lg font-roboto font-normal text-blue-900 py-2">{job.title}</p>
+                  <span className="text-xs font-roboto font-bold text-blue-900 border border-blue-900 rounded py-1 px-2">{job.type}</span>
+                </div>
+                <div className="flex items-end mt-4 sm:mb-2 mr-1">
+                  <span className="flex items-center font-roboto font-medium text-xs text-gray-400 mr-8"><svg className="w-6 h-6 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" /></svg>{job.location}</span>
+                  <span className="flex items-center font-roboto font-medium text-xs text-gray-400"><svg className="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>5 days ago</span>
+                </div>
               </div>
             </div>
-          </div>
+          )) : ''}
         </div>
 
         <div className="col-span-3 lg:col-span-2 flex justify-center lg:justify-end">
