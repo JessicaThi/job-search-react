73360737aa5f03fe403b2c2220cb8aca87e89abf
JessicaThi <jessica.thielemans@hetic.net>
2020-12-01 18:12:56 +0100

    integration page offer + responsive all site

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,18 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main className="container max-w-screen-xl mx-auto">
+    <main className="container max-w-screen-xl mx-auto px-5">
       <h1 className="text-2xl	font-bold py-8">Github <span className="font-light">Jobs</span></h1>
-      <div className="block-search rounded-lg py-6 flex justify-center items-center">
-        <div className="box-search bg-white rounded p-1">
-          <input className="w-96 font-roboto font-normal text-xs outline-none ml-4" type="text" placeholder="Title, companies, expertise or benefits" />
+      <div className="block-search rounded-lg py-6 px-5 flex justify-center items-center">
+        <div className="w-full md:w-11/12 lg:w-9/12 flex justify-between box-search bg-white rounded p-1">
+          <input className="w-full font-roboto font-normal text-xs truncate outline-none mx-4" type="text" placeholder="Title, companies, expertise or benefits" />
           <button className="bg-blue-500 font-roboto text-white text-base font-medium py-3 px-10 rounded">Search</button>
         </div>
       </div>
-      <div className="grid grid-rows-3 grid-flow-col gap-8 pt-8">
-        <aside className="row-span-3">
+      <div className="grid grid-col-1 lg:grid-rows-3 lg:grid-flow-col gap-8 pt-8">
+        <aside className="col-span-3 lg:col-span-1 row-span-3">
           <div className="flex items-center">
-            <input type="checkbox" class="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="full-time" id="full-time" />
+            <input type="checkbox" className="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="full-time" id="full-time" />
             <label className="font-medium text-sm text-blue-900 ml-3" for="full-time">Full-time</label>
           </div>
           <div className="flex flex-col pt-7 pb-4">
@@ -22,40 +22,43 @@ export default function Home() {
               placeholder="City, state, zip code or country" />
           </div>
           <div className="flex items-center pt-2 pb-1">
-            <input type="checkbox" class="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="london" id="london" />
+            <input type="checkbox" className="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="london" id="london" />
             <label className="font-medium text-sm text-blue-900 ml-3" for="london">London</label>
           </div>
           <div className="flex items-center pt-2 pb-1">
-            <input type="checkbox" class="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="amsterdam" id="amsterdam" />
+            <input type="checkbox" className="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="amsterdam" id="amsterdam" />
             <label className="font-medium text-sm text-blue-900 ml-3" for="amsterdam">Amsterdam</label>
           </div>
           <div className="flex items-center pt-2 pb-1">
-            <input type="checkbox" class="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="new-york" id="new-york" />
+            <input type="checkbox" className="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="new-york" id="new-york" />
             <label className="font-medium text-sm text-blue-900 ml-3" for="new-york">New York</label>
           </div>
           <div className="flex items-center pt-2 pb-1">
-            <input type="checkbox" class="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="berlin" id="berlin" />
+            <input type="checkbox" className="form-tick appearance-none h-4 w-4 border border-gray-300 rounded-sm checked:bg-blue-600 checked:border-transparent focus:outline-none" name="berlin" id="berlin" />
             <label className="font-medium text-sm text-blue-900 ml-3" for="berlin">Berlin</label>
           </div>
         </aside>
 
-        <div className="col-span-2">
+        <div className="col-span-3 lg:col-span-2">
           <div className="flex justify-between bg-white p-2 mb-8 rounded shadow-sm">
-            <div className="w-24 h-24 flex justify-center items-center rounded"><img src="https://github-jobs.s3.amazonaws.com/eDYEuyp8KWQ7XhCbzyYkaELK?response-content-disposition=inline%3B%20filename%3D%22Shells%2002.jpg%22%3B%20filename%2A%3DUTF-8%27%27Shells%252002.jpg&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJENXOYUQN2IQEWRA%2F20201128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201128T142305Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=808131e90df6116733341471559a5f3c20411f474e54de5fd49d382529a66499" /></div>
-            <div className="flex flex-1 flex-col items-start">
-              <p className="text-xs font-roboto font-bold text-blue-900">Kasisto</p>
-              <p className="text-lg font-roboto font-normal text-blue-900 py-2">Front-End Software Engineer</p>
-              <span className="text-xs font-roboto font-bold text-blue-900 border border-blue-900 rounded py-1 px-2">Full time</span>
+            <div className="w-24 h-24 flex justify-center items-center rounded">
+              <img src="https://github-jobs.s3.amazonaws.com/eDYEuyp8KWQ7XhCbzyYkaELK?response-content-disposition=inline%3B%20filename%3D%22Shells%2002.jpg%22%3B%20filename%2A%3DUTF-8%27%27Shells%252002.jpg&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJENXOYUQN2IQEWRA%2F20201128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201128T142305Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=808131e90df6116733341471559a5f3c20411f474e54de5fd49d382529a66499" />
             </div>
-            <div className="flex items-end mb-2 mr-1">
-              <span className="flex items-center font-roboto font-medium text-xs text-gray-400 mr-8"><svg className="w-6 h-6 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" /></svg>New York</span>
-              <span className="flex items-center font-roboto font-medium text-xs text-gray-400"><svg className="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>5 days ago</span>
+            <div className="flex flex-1 flex-col sm:flex-row">
+              <div className="flex flex-1 flex-col items-start">
+                <p className="text-xs font-roboto font-bold text-blue-900">Kasisto</p>
+                <p className="text-lg font-roboto font-normal text-blue-900 py-2">Front-End Software Engineer</p>
+                <span className="text-xs font-roboto font-bold text-blue-900 border border-blue-900 rounded py-1 px-2">Full time</span>
+              </div>
+              <div className="flex items-end mt-4 sm:mb-2 mr-1">
+                <span className="flex items-center font-roboto font-medium text-xs text-gray-400 mr-8"><svg className="w-6 h-6 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" /></svg>New York</span>
+                <span className="flex items-center font-roboto font-medium text-xs text-gray-400"><svg className="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>5 days ago</span>
+              </div>
             </div>
           </div>
-          
         </div>
 
-        <div className="col-span-2 flex justify-end">
+        <div className="col-span-3 lg:col-span-2 flex justify-center lg:justify-end">
           <button className="w-9 h-9 mr-3 flex justify-center items-center font-roboto font-normal text-xs text-gray-400 border border-gray-400 rounded hover:text-blue-500 hover:border-blue-500"><svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" /></svg></button>
           <button className="w-9 h-9 mr-3 flex justify-center items-center font-roboto font-normal text-xs text-gray-400 border border-gray-400 rounded hover:text-blue-500 hover:border-blue-500">1</button>
           <button className="w-9 h-9 mr-3 flex justify-center items-center font-roboto font-normal text-xs text-gray-400 border border-gray-400 rounded hover:text-blue-500 hover:border-blue-500">2</button>
---
+++ pages/offer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function offer() {
+  return (
+    <main className="container max-w-screen-xl mx-auto px-5">
+      <h1 className="text-2xl	font-bold py-8">Github <span className="font-light">Jobs</span></h1>
+      <div className="md:flex">
+        <aside className="md:w-1/4 md:pr-20 pb-6 md:pb-0">
+          <a href="#" className="flex items-center font-poppins font-medium text-sm text-blue-500"><svg className="w-6 h-6 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" /></svg>Back to search</a>
+          <p className="font-bold uppercase text-sm text-gray-400 pt-9 pb-4">How to Apply</p>
+          <p className="font-poppins font-medium text-sm text-blue-900">Please email a copy of your resume and online portfolio to wes@kasisto.com & CC eric@kasisto.com</p>
+        </aside>
+        <div className="md:w-3/4 pb-6">
+          <p className="text-2xl font-roboto font-bold text-blue-900 py-2">Front-End Software Engineer <span className="ml-3 text-xs font-roboto font-bold text-blue-900 align-middle border border-blue-900 rounded py-1 px-2">Full time</span></p>
+          <span className="flex items-center font-roboto font-medium text-xs text-gray-400"><svg className="w-6 h-6 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>5 days ago</span>
+          <div className="flex py-6">
+            <div className="w-11 h-11 mr-2 flex justify-center items-center rounded"><img src="https://github-jobs.s3.amazonaws.com/eDYEuyp8KWQ7XhCbzyYkaELK?response-content-disposition=inline%3B%20filename%3D%22Shells%2002.jpg%22%3B%20filename%2A%3DUTF-8%27%27Shells%252002.jpg&response-content-type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJENXOYUQN2IQEWRA%2F20201128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20201128T142305Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=808131e90df6116733341471559a5f3c20411f474e54de5fd49d382529a66499" /></div>
+            <div>
+              <p className="text-lg font-roboto font-bold text-blue-900">Kasisto</p>
+              <span className="flex items-center font-roboto font-medium text-xs text-gray-400 mr-8"><svg className="w-5 h-5 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd" /></svg>New York</span>
+            </div>
+          </div>
+          <div className="font-roboto font-normal text-blue-900">
+            <p>We as GOhiring believe that connectivity is the foundation of efficient processes. Therefore we offer the digital infrastructure for HR Market Players to provide a productive way of collaborating. Our software solution enables customers to publish, manage and analyze their job postings within their HR software. Thanks to state-of-the-art tools, we have also digitized our own working environment. Everything is set up for #remote work from anywhere in the world.</p><p><strong>Your role</strong></p><ul><li>Build smart, customer-driven front- and back-end solutions in extension of our product ecosystem</li><li>Solve exciting engineering challenges, starting with MVP’s and iterate from there</li><li>Work on complex frontend projects using React and other JS frameworks</li><li>Collaborate closely with our experienced team of backend developers, product managers, designers and testers</li></ul><p><strong>Your profile</strong></p><ul><li>You get excited about good quality code and engineering quests</li><li>+5 years experience in web development, including +1 years in React.js and +3 years in Ruby</li><li>Value testing, well-documented code and intelligent software design techniques</li><li>Profound knowledge about API Design and performance optimization</li><li>Fluent in English</li></ul><p><strong>Why should you apply?</strong></p><p><em>Remote culture</em> - Work wherever you are most productive - at home, while traveling or in our office in Berlin. Benefit from processes and mindsets which are 100% oriented towards working flexible hours.</p><p><em>Small, agile team</em> - We believe that work is simply more fun in small teams. Experienced colleagues, jointly defined company targets, a direct exchange and lots of creative leeway: That is what constitutes our understanding of productivity.</p><p><em>Complex challenges</em> - We encounter new challenges time and time which are waiting to be solved for the benefit of all market players.</p><p><strong>Sounds like an interesting challenge for you?</strong></p><p>Simply send us your profile in addition to your Github handle. We are happy to hear from you.</p><p></p>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
