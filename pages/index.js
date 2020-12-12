fd96ccc6c7b69f0b23394e3b6efe5c81f02bb363
JessicaThi <jessica.thielemans@hetic.net>
2020-12-12 22:03:11 +0100

    update css

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ export default function Home() {
       <h1 className="text-2xl	font-bold py-8">Github <span className="font-light">Jobs</span></h1>
       <div className="block-search rounded-lg py-6 px-5 flex justify-center items-center">
         <div className="w-full md:w-11/12 lg:w-9/12 flex justify-between items-center box-search bg-white rounded p-1 focus-within:text-gray-600">
-          <svg className="flex-shrink-0 w-6 h-6 ml-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
+          <svg className="flex-shrink-0 w-5 h-5 ml-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>
           <input onChange={onChangeDescription} value={searchDescription} className="w-full font-roboto font-normal text-xs truncate outline-none ml-2 mr-4" type="text" placeholder="Title, companies, expertise or benefits" />
           <button onClick={() => {
             setChangeDescription(searchDescription);
